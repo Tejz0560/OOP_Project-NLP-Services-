9c7972802b67bb7a27a4e5831c8420e00674f286
Tejz0560 <tejbidwai@gmail.com>
2022-12-03 22:25:47 +0530

    Login and Register Gui added

--- app.py
+++ app.py
@@ -11,8 +11,13 @@ class NLPAPP:
         self.login_gui()
         self.root.mainloop()
 
-    
+    def clear(self):
+        for i in self.root.pack_slaves():
+            i.destroy()
+
+
     def login_gui(self):
+        self.clear()
 
         heading = Label(self.root,text='NLP App',bg='#444343',fg='white')
         heading.pack(pady=(30,30))
@@ -27,7 +32,7 @@ class NLPAPP:
         label2 = Label(self.root,text='Enter Password')
         label2.pack(pady=(10,10))
 
-        self.password_input = Entry(self.root,width=50)
+        self.password_input = Entry(self.root,width=50,show='*')
         self.password_input.pack(pady=(5,10),ipady=4)
 
         login_btn = Button(self.root,text="Login",width=30,height=2)
@@ -36,7 +41,44 @@ class NLPAPP:
         label3 = Label(self.root,text='Not a Member?')
         label3.pack(pady=(10,10))
 
-        redirect_btn = Button(self.root,text="Register",width=10,height=1)
+        redirect_btn = Button(self.root,text="Register",width=10,height=1, command=self.register_gui)
         redirect_btn.pack(pady=(10,10))
+
+
+    def register_gui(self):
+        
+        self.clear()
+
+        heading = Label(self.root,text='NLP App',bg='#444343',fg='white')
+        heading.pack(pady=(30,30))
+        heading.configure(font=('verdana',24,'bold'))
+
+        label0 = Label(self.root,text='Enter Name')
+        label0.pack(pady=(10,10))
+
+        self.name_input = Entry(self.root,width=50)
+        self.name_input.pack(pady=(5,10),ipady=4)
+
+        label1 = Label(self.root,text='Enter Email')
+        label1.pack(pady=(10,10))
+
+        self.email_input = Entry(self.root,width=50)
+        self.email_input.pack(pady=(5,10),ipady=4)
+
+        label2 = Label(self.root,text='Enter Password')
+        label2.pack(pady=(10,10))
+
+        self.password_input = Entry(self.root,width=50,show='*')
+        self.password_input.pack(pady=(5,10),ipady=4)
+
+        register_btn = Button(self.root,text="Register",width=30,height=2)
+        register_btn.pack(pady=(10,10))
+
+        label3 = Label(self.root,text='Already a Member?')
+        label3.pack(pady=(10,10))
+
+        redirect_btn = Button(self.root,text="Login",width=10,height=1, command=self.login_gui)
+        redirect_btn.pack(pady=(10,10))
+
 
 nlp = NLPAPP()
